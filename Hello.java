27f1004c66c3b3411f13618b7a2cf459e4ed348e
Dyah Mustikasari <120073741+DYAHMUSTIKASARI@users.noreply.github.com>
2023-10-09 14:29:15 +0700

    Update Hello.java
    pembetulan di apostrope

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
  
 public static void main(String[] args) { 
-           System.out.print(“Nama saya Budi!”);
+           System.out.print("Nama saya Budi!");
  
-     System.out.println(“Selamat datang Java!”); 
+     System.out.println("Selamat datang Java!"); 
      }
 }

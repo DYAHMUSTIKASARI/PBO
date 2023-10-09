8ee9ebd11f84a0e8e8b9b573199ec4121f326df5
DYAHMUSTIKASARI <120073741+DYAHMUSTIKASARI@users.noreply.github.com>
2023-10-09 13:05:05 +0700

    Create CetakWaktu.java
    Kode ini merupakan contoh penggunaan operasi pada variabel yang telah ditentukan

---
+++ CetakWaktu.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+public class CetakWaktu { 
+	public static void main(String[] args) { 
+		Scanner input = new Scanner(System.in); 
+		System.out.print (“Masukkan waktu dalam detik: ”); 
+		int detik = input.nextInt(); 
+		int menit = detik / 60; 
+		int sisadetik = detik % 60; 
+		System.out.print(detik+ “ detik adalah ”+menit+ ” menit ”+sisadetik+” detik.”);
+	}
+}

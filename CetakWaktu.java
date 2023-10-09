c1012edc28f4da4e62fe779a9270c898424866bb
Dyah Mustikasari <120073741+DYAHMUSTIKASARI@users.noreply.github.com>
2023-10-09 14:34:22 +0700

    Update CetakWaktu.java
    pembetulan tanda petik (")

--- CetakWaktu.java
+++ CetakWaktu.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class CetakWaktu { 
 	public static void main(String[] args) { 
 		Scanner input = new Scanner(System.in); 
-		System.out.print (“Masukkan waktu dalam detik: ”); 
+		System.out.print ("Masukkan waktu dalam detik: "); 
 		int detik = input.nextInt(); 
 		int menit = detik / 60; 
 		int sisadetik = detik % 60; 
-		System.out.print(detik+ “ detik adalah ”+menit+ ” menit ”+sisadetik+” detik.”);
+		System.out.print(detik+ " detik adalah "+menit+ " menit "+sisadetik+" detik.");
 	}
 }

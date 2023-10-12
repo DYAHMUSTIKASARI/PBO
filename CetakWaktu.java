e9ba0e79bfb4f23c079773e7a7426d204b6da424
Dyah Mustikasari <120073741+DYAHMUSTIKASARI@users.noreply.github.com>
2023-10-12 15:04:00 +0700

    Update CetakWaktu.java

--- CetakWaktu.java
+++ CetakWaktu.java
@@ -3,6 +3,7 @@ public class CetakWaktu {
 	public static void main(String[] args) { 
 		Scanner input = new Scanner(System.in); 
 		System.out.print ("Masukkan waktu dalam detik: "); 
+		
 		int detik = input.nextInt(); 
 		int menit = detik / 60; 
 		int sisadetik = detik % 60; 

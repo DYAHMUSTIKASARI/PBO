93c78390ab6249a9afb2a4f10b1d38ee003204a4
Dyah Mustikasari <120073741+DYAHMUSTIKASARI@users.noreply.github.com>
2024-01-29 12:44:12 +0700

    Update AbstractCRUD.java

--- JavaGUI/AbstractCRUD.java
+++ JavaGUI/AbstractCRUD.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package crud;
 
-/**
- *
- * @author Izzuel
- */
 public abstract class AbstractCRUD {
     protected String id, nama, alamat;
 

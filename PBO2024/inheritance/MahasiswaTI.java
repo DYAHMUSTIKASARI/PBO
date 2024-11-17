6db91ad4c93416e8da2f1888eec389a37aca9a88
Dyah Mustikasari <120073741+DYAHMUSTIKASARI@users.noreply.github.com>
2024-11-17 17:31:16 +0700

    Create MahasiswaTI.java

---
+++ PBO2024/inheritance/MahasiswaTI.java
@@ -0,0 +1,17 @@
+package pbo2024.satu;
+
+
+public class MahasiswaTI extends Mahasiswa{
+    String prodi;
+    
+public MahasiswaTI(int NIM, String nama, char kelas, double ipk, String prodi){
+    super(NIM, nama, kelas, ipk);
+    this.prodi = prodi;
+    }
+
+@Override
+public void tampilData() {
+        super.tampilData();
+        System.out.println("Program Studi : " + prodi); 
+    }
+}
